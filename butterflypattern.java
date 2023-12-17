be8bf04ef2025d606b116c813d6183cf0e354c00
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-12-17 22:32:33 +0530

    Update butterflypattern.java

--- butterflypattern.java
+++ butterflypattern.java
@@ -4,45 +4,45 @@ public class butterflypattern{
 
     public static void main(String[] args){
 
-        int n=5;
+        int n = 5;
         
-        //upper part--->
+        // for upper part--->
 
-        for(int i=1; i<=n; i++){
-             for(int j=1; j<=i; j++){
+        for(int i = 1; i <= n; i++){
+             for(int j = 1; j <= i; j++){
                 System.out.print("*");
              }
 
-             //spaces-->
+             // for spaces-->
 
-               int spaces= 2*(n-i);
-               for(int j=1; j<=spaces; j++){
+               int spaces= 2 *  (n - i);
+               for(int j = 1; j <= spaces; j++){
                 System.out.print(" ");
                }
-             //2nd part--->
+             // for 2nd part--->
 
-                for(int j=1; j<=i; j++){
+                for(int j = 1; j <= i; j++){
                     System.out.print("*");
                    }
                System.out.println();
         }
 
-        //lower part--->
+        // for lower part--->
         
-        for(int i=n; i>=1; i--){
-            for(int j=1; j<=i; j++){
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
                System.out.print("*");
             }
 
-            //spaces-->
+            // for spaces-->
 
-              int spaces= 2*(n-i);
-              for(int j=1; j<=spaces; j++){
+              int spaces = 2 * (n - i);
+              for(int j = 1; j <= spaces; j++){
                System.out.print(" ");
               }
-            //2nd part--->
+            // for 2nd part--->
 
-               for(int j=1; j<=i; j++){
+               for(int j = 1; j <= i; j++){
                    System.out.print("*");
                   }
               System.out.println();
