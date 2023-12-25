31b4819042e1662b3c2bf82fd0fc576187f587f3
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-12-25 17:28:47 +0530

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Function {
+    public static void printName(String name){
+        System.out.print("My name is");
+        System.out.println(name);
+        return;
+    }
+
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    String name = sc.next();
+
+    printName(name);
+}
+}
+
