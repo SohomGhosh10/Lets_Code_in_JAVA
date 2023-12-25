ad7cb08b3587b2882ed4ffd949de8ee519c57fcf
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-12-25 18:13:06 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 public class Factorial {
     public static void CalculateFactorial(int n){
+        if(n < 0){
+            System.out.println("Invalid Number");
+            return;
+        }
         int factorial = 1;
         for(int i = n; i >= 1; i--){
             factorial = factorial * i;
