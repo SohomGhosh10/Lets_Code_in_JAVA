ffce81440d668fa04aca6f160ef99181c7ff6630
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2024-01-06 14:10:20 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Arrays{
+    public static void main(String[] args){
+
+        // Declaring array of integer type
+        int[] marks = new int[3];
+        marks[0] = 97;
+        marks[1] = 90;
+        marks[2] = 95;
+
+        System.out.println("Marks are");
+        
+        // Displaying marks
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+}
