1fa516ed33424abefbc3b54f8744b733976ab77d
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2024-01-13 13:53:05 +0530

    Create Problem.java

---
+++ Problem.java
@@ -0,0 +1,10 @@
+public class project2 {
+    public static void main(String[] args) {
+        float y;
+        y = 7 / 4 * 9 / 2;
+        float x = 7 / 4f * 9 / 2f;
+        System.out.println(y); // 4
+        System.out.println(x); // 7.875
+    }
+
+}
