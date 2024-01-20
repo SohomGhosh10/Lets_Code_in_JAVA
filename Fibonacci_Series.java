6ccdf60c02cf2b9a877c752a98a8d97995398f9f
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2024-01-20 15:36:46 +0530

    Update Fibonacci_Series.java

--- Fibonacci_Series.java
+++ Fibonacci_Series.java
@@ -6,7 +6,7 @@ public class Fibonacci_Series {
         int n = sc.nextInt();
 
         int a = 0, b = 1;
-        System.out.print(a + " ");
+        System.out.print(a + " "); // fibonacci series
         if(n > 1){
             for(int i = 2; i <= n; i++){
                 System.out.print(b + " ");
