f5196754ae88955176a2ab0032c422ec4356f5b0
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2024-01-29 20:37:18 +0530

    Add files via upload

---
+++ Fibonacci_Recursion.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Fibonacci_Recursion {
+
+    public static class Fibonacci{
+        static int CalFibo(int num){
+            if(num <= 1){
+                return 1;
+            }
+            else{
+                return CalFibo(num - 1) + CalFibo(num - 2);
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        Fibonacci fibo = new Fibonacci();
+        //int result = fibo.CalFibo(num);
+
+        for(int i = 0; i < num; i++) {
+            System.out.println(fibo.CalFibo(i) + " ");
+        }
+    }
+}
