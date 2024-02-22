c9362284a03ba13f8eb51e57ac8e795c7899a80b
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2024-02-22 20:41:16 +0530

    Update Constructor_Example2.java

--- Constructor_Example2.java
+++ Constructor_Example2.java
@@ -3,7 +3,7 @@ class Box{
     double height;
     double depth;
 
-    public Box(double w , double h , double d){
+    public Box(double w , double h , double d){ // parameterized constructor
         this.width = w;
         this.height = h;
         this.depth = d;
@@ -16,10 +16,10 @@ class Box{
 
 public class Constructor_Example2 {
     public static void main(String[] args) {
-        Box b1 = new Box(10 , 20 , 15);
+        Box b1 = new Box(10 , 20 , 15); // sending parameters
         Box b2 = new Box(3 , 6 , 9);
 
-        double vol1 = b1.volume();
+        double vol1 = b1.volume(); // accessing the methods
         System.out.println(vol1);
 
         double vol2 = b2.volume();
