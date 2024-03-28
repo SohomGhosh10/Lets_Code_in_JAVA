69244587f41aedbaceb196383d0c3c1849ce1824
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2024-03-28 23:28:50 +0530

    Update Sort_Zeros.java

--- Sort_Zeros.java
+++ Sort_Zeros.java
@@ -6,7 +6,7 @@ public class Sort_Zeros {
         int n = arr.length;
         SortZeros(arr , n);
         for(int i = 0; i < arr.length; i++){
-            System.out.println(arr[i] + " "); // 3 5 2 0 0
+            System.out.println(arr[i] + " "); // 3 5 2 0 0.
         }
     }
 
