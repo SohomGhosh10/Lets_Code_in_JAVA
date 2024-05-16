bd1905d5331c5ee1f170816a6b47eab7d265464c
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2024-05-16 16:08:28 +0530

    Update ArrayListExample.java

--- ArrayListExample.java
+++ ArrayListExample.java
@@ -8,7 +8,7 @@ public class ArrayListExample {
         l1.add(4);
         l1.add(3);
         l1.add(2);
-        l1.add(1);
+        l1.add(0 , 1); // Put 1 at index 0
 
         for(int i = 0; i < l1.size(); i++){
             System.out.println(l1.get(i));
