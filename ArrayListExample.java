600f9a855424f35136e73b19479bc5ad97ff0ea8
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2024-05-16 15:56:52 +0530

    Add files via upload

---
+++ ArrayListExample.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ArrayListExample {
+    public static void main(String[] args){
+        ArrayList<Integer> l1 = new ArrayList<>();
+        l1.add(6);
+        l1.add(5);
+        l1.add(4);
+        l1.add(3);
+        l1.add(2);
+        l1.add(1);
+
+        for(int i = 0; i < l1.size(); i++){
+            System.out.println(l1.get(i));
+        }
+    }
+}
