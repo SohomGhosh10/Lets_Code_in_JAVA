ec5cf156087aff660d88f576016352afa39f602f
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2024-08-22 23:13:14 +0530

    Update HashSetExample.java

--- HashSetExample.java
+++ HashSetExample.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class HashSetExample {
     public static void main(String[] args) {
         HashSet<String> hs = new HashSet<String>();
+        HashSet<Integer> set = new HashSet<Integer>();
         /*
          * HashSet Features:
          * 1. Contains unique elements
@@ -32,5 +33,22 @@ public class HashSetExample {
           hs.clear();
           System.out.println(hs);
           System.out.println("HashSet is empty? " + hs.isEmpty());  
+
+          set.add(1);
+          set.add(2);
+          set.add(3);
+          set.add(4);
+          set.add(5);
+          set.add(6);
+          set.add(7);
+
+          System.out.println(set);
+
+          Iterator it = set.iterator();
+          // hasNext() and next()
+
+          while(it.hasNext()){
+            System.out.println(it.next());
+          }
     }
 }
